3ce5045c0ef29d3a56965aefd24ed7844efc20d2
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-25 20:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ let hours = CurrentDate.getHours();
 const minute = CurrentDate.getMinutes();
 const second = CurrentDate.getSeconds();
 let TimeSatmp = "";
-if(hours>12){
-hours=hours%12;
-TimeSatmp = "PM";
-}
-else{
+
+if (hours > 12) {
+	hours = hours%12;
+	TimeSatmp = "PM";
+} else {
   TimeSatmp = "AM";
 }
+
 DtTime = `${month}/${Day}/${year}, ${hours}:${minute}:${second} ${TimeSatmp}`
 time.innerHTML = DtTime;
